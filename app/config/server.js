d1277e54688ffbb7cccfe404e9662f1c3156d60c
Alisson Alcântara Amaral da Silva - ASV <alisson.silva@iteris.com.br>
2021-02-15 20:08:40 -0300

    first commit

---
+++ app/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'd83d9a28ac20deb534af6ccac61353d7'),
+    },
+  },
+});
---
+++ app/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '32e5e07c-1b2a-4a79-a29e-6c22bb82ddef'
+};
